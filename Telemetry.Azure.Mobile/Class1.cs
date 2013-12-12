3a6ce4e37b7a0793cece8575764c052c703889f0
Shane Chism <hey@shanechism.com>
2013-12-11 22:06:13 -0800

    Re-fixed references?

--- Telemetry.Azure.Mobile/Class1.cs
+++ Telemetry.Azure.Mobile/Class1.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.WindowsAzure.MobileServices;
 
 namespace Telemetry.Azure.Mobile
 {
-    public class Class1
-    {
-    }
+	public class Class1 {
+
+		public Class1() {
+
+			
+
+		}
+
+	}
 }
