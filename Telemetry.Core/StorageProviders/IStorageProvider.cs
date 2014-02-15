26b987afc854713908322fd5959e290a55507c52
Shane Chism <hey@shanechism.com>
2014-02-15 15:47:40 -0800

    Updating repo to match files

---
+++ Telemetry.Core/StorageProviders/IStorageProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Telemetry.StorageProviders {
+
+	public interface IStorageProvider {
+
+		byte[] GetData( string targetFile );
+		void SaveData( byte[] data );
+
+	}
+
+}
---
+++ Telemetry.WindowsPhone/StorageProviders/PhoneFileSystem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telemetry;
+
+namespace Telemetry.StorageProviders {
+
+	public class PhoneFileSystem : IStorageProvider {
+
+		public byte[] GetData( string targetFile ) {
+
+			using( var storage = IsolatedStorageFile.GetUserStoreForApplication() ) {
+
+				if( !storage.FileExists( targetFile ) )
+					return null;
+
+				using( var file = storage.OpenFile( targetFile, FileMode.Open, FileAccess.Read, FileShare.Read ) ){
+					var data = new byte[file.Length];
+					file.Read( data, 0, data.Length );
+					return data;
+				}
+
+			}
+
+		}
+
+		public void SaveData( byte[] data ) {
+			throw new NotImplementedException();
+		}
+	}
+
+}
