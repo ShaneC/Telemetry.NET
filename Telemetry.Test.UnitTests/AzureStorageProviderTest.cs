d9cfa7b638006d3888a2b7df984197691a195bf3
Shane Chism <hey@shanechism.com>
2014-03-09 21:33:37 -0700

    Fortified unit tests to check and ensure row is present during Azure Storage add of ErrorReport

--- Telemetry.Test.UnitTests/AzureStorageProviderTest.cs
+++ Telemetry.Test.UnitTests/AzureStorageProviderTest.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Xml.Linq;
 using Telemetry.Core;
 using Telemetry.StorageProviders;
@@ -10,10 +13,12 @@ namespace Telemetry.Test.UnitTests {
 	[TestClass]
 	public class AzureStorageProviderTest {
 
+		private static string PartitionKey = "error";
+
 		private static AzureTableStorageProvider AzureStorageEmulator = new AzureTableStorageProvider( new AzureTableStorageSettings {
 			ConnectionString = "UseDevelopmentStorage=true",
 			TableName = "reports",
-			PartitionKey = "error",
+			PartitionKey = PartitionKey,
 			SchemaDefinition = AzureTableStorageProvider.GetDefaultSchema()
 		} );
 
@@ -48,24 +53,59 @@ namespace Telemetry.Test.UnitTests {
 		[TestMethod]
 		public void AzureStorageProvider_Save() {
 
+			ClearAzureTable( PartitionKey ).Wait();
+
 			XDocument schema = AzureTableStorageProvider.GetDefaultSchema();
 
-			AzureTableStorageProvider storage = new AzureTableStorageProvider( new AzureTableStorageSettings {
-				ConnectionString = "UseDevelopmentStorage=true",
-				TableName = "reports",
-				PartitionKey = "error",
-				SchemaDefinition = schema
-			} );
-
 			TelemetryClient client = new TelemetryClient();
+			ErrorReport report;
 
 			try {
 				throw new GenericException( ErrorCode.GENERIC_CRITICAL_ERROR, "Test Exception Message" );
 			} catch( Exception e ) {
-				client.AddActiveReport( new ErrorReport( e ) );
+				report = new ErrorReport( e );
+				client.AddActiveReport( report );
 			}
 
 			client.UploadActiveReportsAsync( AzureStorageEmulator ).Wait();
+
+			TableQuery<DynamicTableEntity> retrieve = new TableQuery<DynamicTableEntity>().Where(
+				TableQuery.GenerateFilterCondition( "PartitionKey", QueryComparisons.Equal, PartitionKey )
+			);
+
+			var entities = AzureStorageEmulator.StorageTable.ExecuteQuery( retrieve );
+
+			Assert.AreEqual( report.ActivityTime.ToString( "o" ), entities.First().Properties["_ActivityTime"].StringValue );
+			
+			ClearAzureTable( PartitionKey ).Wait();
+
+		}
+
+		private Task<bool> ClearAzureTable( string partitionKey ) {
+
+			TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
+
+			TableQuery<DynamicTableEntity> retrieve = new TableQuery<DynamicTableEntity>().Where(
+				TableQuery.GenerateFilterCondition( "PartitionKey", QueryComparisons.Equal, partitionKey )
+			);
+
+			var entities = AzureStorageEmulator.StorageTable.ExecuteQuery( retrieve );
+
+			if( entities.Count() < 1 ) {
+				tcs.SetResult( true );
+				return tcs.Task;
+			}
+
+			TableBatchOperation batch = new TableBatchOperation();
+
+			foreach( var entity in entities )
+				batch.Add( TableOperation.Delete( entity ) );
+
+			AzureStorageEmulator.StorageTable.BeginExecuteBatch( batch, response => {
+				tcs.SetResult( true );
+			}, null );
+
+			return tcs.Task;
 
 		}
 
