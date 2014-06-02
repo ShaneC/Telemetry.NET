545ab04a6174d6c0e94db4105a34ba1becef2c5b
Shane Chism <hey@shanechism.com>
2014-06-01 20:32:38 -0700

    Adding in additional data to TestConfig

--- Telemetry.Test.TestLibrary/TestConfig.cs
+++ Telemetry.Test.TestLibrary/TestConfig.cs
@@ -4,25 +4,30 @@ namespace Telemetry.Test.TestLibrary {
 	public static class TestConfig {
 
 		/// <summary>
+		/// Azure Storage Account Name
+		/// </summary>
+		public static string AzureTestAccountName = "";
+
+		/// <summary>
 		/// Uri for the table path of your Azure Storage account
 		/// </summary>
 		/// <example>https://<youraccountname>.table.core.windows.net</example>
-		public static readonly string AzureTestAccountUri = "";
+		public static string AzureTestAccountUri = "";
 
 		/// <summary>
 		/// Table name that cooresponds to the AzureTestTableSAS provided
 		/// </summary>
-		public static readonly string AzureTestTableName = "";
+		public static string AzureTestTableName = "";
 
 		/// <summary>
 		/// Query String for Azure Shared Access Signature (SAS) -- not the full URI (string begins with ?)
 		/// </summary>
-		public static readonly string AzureTestTableSAS = "";
+		public static string AzureTestTableSAS = "";
 
 		/// <summary>
 		/// Account Key cooresponding to the target AzureTestAccountUri
 		/// </summary>
-		public static readonly string AzureTestAccountKey = "";
+		public static string AzureTestAccountKey = "";
 
 	}
 
