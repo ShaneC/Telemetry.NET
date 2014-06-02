4349271f25a1fbd635a5df3f4b3cf36c90c99497
Shane Chism <hey@shanechism.com>
2014-06-01 20:03:26 -0700

    Adding TestConfig

---
+++ Telemetry.Test.TestLibrary/TestConfig.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Telemetry.Test.TestLibrary {
+
+	public static class TestConfig {
+
+		/// <summary>
+		/// Uri for the table path of your Azure Storage account
+		/// </summary>
+		/// <example>https://<youraccountname>.table.core.windows.net</example>
+		public static readonly string AzureTestAccountUri = "";
+
+		/// <summary>
+		/// Table name that cooresponds to the AzureTestTableSAS provided
+		/// </summary>
+		public static readonly string AzureTestTableName = "";
+
+		/// <summary>
+		/// Query String for Azure Shared Access Signature (SAS) -- not the full URI (string begins with ?)
+		/// </summary>
+		public static readonly string AzureTestTableSAS = "";
+
+		/// <summary>
+		/// Account Key cooresponding to the target AzureTestAccountUri
+		/// </summary>
+		public static readonly string AzureTestAccountKey = "";
+
+	}
+
+}
