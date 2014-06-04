c89e0b9c30070b984ccfebd36dab2cb1821e043f
Shane Chism <hey@shanechism.com>
2014-06-04 14:47:43 -0700

    Whoops

--- Telemetry.Test.WP/TestConfig.cs
+++ Telemetry.Test.WP/TestConfig.cs
@@ -6,7 +6,7 @@ namespace Telemetry.Test.TestLibrary {
 		/// <summary>
 		/// Account Name
 		/// </summary>
-		public static string AzureTestAccountName = "vaporapp";
+		public static string AzureTestAccountName = "";
 
 		/// <summary>
 		/// Uri for the table path of your Azure Storage account
@@ -17,17 +17,17 @@ namespace Telemetry.Test.TestLibrary {
 		/// <summary>
 		/// Table name that cooresponds to the AzureTestTableSAS provided
 		/// </summary>
-		public static string AzureTestTableName = "clienttelemetrytest";
+		public static string AzureTestTableName = "";
 
 		/// <summary>
 		/// Query String for Azure Shared Access Signature (SAS) -- not the full URI (string begins with ?)
 		/// </summary>
-		public static string AzureTestTableSAS = "?sv=2014-02-14&tn=clienttelemetrytest&sig=cADiT%2BJgtWUptdFMhTTh0FpKJ%2FFnQ4jNZSQPxo7ZANo%3D&se=2016-05-29T20%3A45%3A39Z&sp=raud";
+		public static string AzureTestTableSAS = "";
 
 		/// <summary>
 		/// Account Key cooresponding to the target AzureTestAccountUri
 		/// </summary>
-		public static string AzureTestAccountKey = "eqapzBzdtx1C3hBpdBs81GVDiVwOSOK75trABDDKZWSMggHidOFJzmYe9oIztFYdkZ1DtIZ204+M3+xmUsiNNg==";
+		public static string AzureTestAccountKey = "";
 
 	}
 
